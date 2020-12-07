4ae8b604180a3b64a7ab65d45006d468a9a84479
syo0306 <yama-syo.0306@ezweb.ne.jp>
2020-12-07 18:59:00 +0900

    ユーザー２

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 | first_name_kana      | string | null: false              |
 | last_name            | string | null: false              |
 | last_name_kana       | string | null: false              |
-| birthday             | date   | null: false              |
+| birth_day             | date   | null: false              |
 
 ### Association
 
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    it "nicknameが空だと登録できない" do
+    end
+
+    it "emailが空では登録できない" do
+    end
+
+    it "encrypted_passwordが空では登録できない" do
+    end
+
+    it "first_nameが空では登録できない" do
+    end
+
+    it "first_name_kanaが空では登録できない" do
+    end
+
+    it "last_nameが空では登録できない" do
+    end
+
+    it "last_name_kanaが空では登録できない" do
+    end
+
+    it "birth_dayが空では登録できない" do
+    end
+  end
+end
+
