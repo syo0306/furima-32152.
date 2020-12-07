bb0ce2887d754087e887c15f1f5dadad525c92fe
syo0306 <yama-syo.0306@ezweb.ne.jp>
2020-12-07 18:34:26 +0900

    ユーザー１

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
+
 
   private
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['sy0306'] && password == ENV['0306']
+      username == 'sy0306' && password == '0306'
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :first_name, :first_name_kana, :last_name, :last_name_kana, :birth_day])
   end
 end
