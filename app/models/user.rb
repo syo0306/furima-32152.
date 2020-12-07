423f61855ca6b8d58202f7634aa21fc9fbf57f17
syo0306 <yama-syo.0306@ezweb.ne.jp>
2020-12-07 20:09:16 +0900

    ユーザー２

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :encrypted_password, presence: true
-  validates :first_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :birth_day, presence: true
+
+ 
+   validates :nickname, presence: true
+   validates :email, presence: true
+   validates :encrypted_password, presence: true
+   validates :first_name, presence: true
+   validates :first_name_kana, presence: true
+   validates :last_name, presence: true
+   validates :last_name_kana, presence: true
+   validates :birth_day, presence: true
+
 
   has_many :items
   has_many :orders
