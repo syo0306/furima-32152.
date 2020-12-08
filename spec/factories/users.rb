e8ea75377d919df04e11fa8a572e2a02592424d5
syo0306 <yama-syo.0306@ezweb.ne.jp>
2020-12-08 17:28:54 +0900

    ユーザー５

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,10 +3,10 @@ FactoryBot.define do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
-    first_name
-    first_name_kana
-    last_name
-    last_name_kana
-    birth_day
+    first_name            {Faker::Internet.first_name}
+    first_name_kana       {Faker::Internet.first_name_kana}
+    last_name             {Faker::Internet.last_name}
+    last_name_kana        {Faker::Internet.last_name_kana}
+    birth_day             {Faker::Internet.birth_bay}
   end
 end
