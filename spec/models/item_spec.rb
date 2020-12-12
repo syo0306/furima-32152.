292495838e1dc3d724b8ed49ffdc59bda95dc97d
syo0306 <yama-syo.0306@ezweb.ne.jp>
2020-12-12 17:54:13 +0900

    商品出品１０

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -69,13 +69,13 @@ RSpec.describe Item, type: :model do
     end
 
     it "価格のが、300円以上でないと登録できない" do
-      @item.item_price = "10"
+      @item.item_price = 10
       @item.valid?
       expect(@item.errors.full_messages).to include("Item price is out of setting range")
     end
 
     it "価格が、10,000,000円以上では登録ができない" do
-      @item.item_price = "10000000"
+      @item.item_price = 10000000
       @item.valid?
       expect(@item.errors.full_messages).to include("Item price is out of setting range")
     end
