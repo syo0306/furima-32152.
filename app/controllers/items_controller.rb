e19edaad33dc4d07204216adfadf00a17143a916
syo0306 <yama-syo.0306@ezweb.ne.jp>
2020-12-14 12:43:02 +0900

    商品詳細１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,12 +18,18 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
-    
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
   def item_params
     params.require(:item).permit(:name, :text, :image, :item_price, :category_id, :status_id, :area_id, :item_postage_id, :day_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :edit
+    end
   end
 end
