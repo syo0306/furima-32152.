afa74ed0da6c507cdb97848da22b655c65022b49
syo0306 <yama-syo.0306@ezweb.ne.jp>
2020-12-15 15:56:11 +0900

    商品情報３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create]
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update]
+  before_action :set_item, only:[:show, :edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -19,29 +20,30 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     unless user_signed_in?
       redirect_to user_session_path
     end
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    if current_user.id
+    if @item.save
       redirect_to item_path
-    else
+    elsif
       render :edit
     end
   end
 
+
   private
   def item_params
     params.require(:item).permit(:name, :text, :image, :item_price, :category_id, :status_id, :area_id, :item_postage_id, :day_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def move_to_index
