006689831100776edd006d1dca550dc2ee428d9e
syo0306 <yama-syo.0306@ezweb.ne.jp>
2020-12-16 12:20:55 +0900

    商品削除３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,8 +37,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if current_user.id = @item.user.id
+     @item.destroy
+     redirect_to root_path
+    end
   end
 
   private
