485e8c3857b70b25c228a928ef4198936332aa98
syo0306 <yama-syo.0306@ezweb.ne.jp>
2020-12-30 18:46:46 +0900

    編集７

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
   devise_for :users
   root to: "items#index"
   resources :items
    resources :orders, only: [:index, :create]
 end
-=======
-  
 end
->>>>>>> Stashed changes
